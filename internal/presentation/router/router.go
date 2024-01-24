20a71f4afcb51535d928cda27dd9bf57095dbfeb
OddEer0 <oddeer0@gmail.com>
2024-01-24 19:32:40 +0300

    [feature-8693kg3fc]: add error middleware

--- internal/presentation/router/router.go
+++ internal/presentation/router/router.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	appErrors "github.com/OddEer0/task-manager-server/internal/common/lib/app_errors"
+	"github.com/OddEer0/task-manager-server/internal/infrastructure/logger"
 	"github.com/OddEer0/task-manager-server/internal/presentation/handlers"
 	"github.com/go-chi/chi/v5"
 )
@@ -8,8 +10,10 @@ import (
 func AppRouter(router *chi.Mux) {
 	appHandler := handlers.NewAppHandler()
 
+	middleware := appErrors.LoggingMiddleware(logger.NewLogger())
+
 	router.Route("/http/v1", func(router chi.Router) {
-		router.Post("/auth/registration", appHandler.Registration)
-		router.Post("/auth/login", appHandler.Login)
+		router.Post("/auth/registration", middleware(appHandler.Registration))
+		router.Post("/auth/login", middleware(appHandler.Login))
 	})
 }
