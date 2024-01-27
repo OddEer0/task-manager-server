65fbd122093c295e1878303dcdd9f8ccdc112376
OddEer0 <oddeer0@gmail.com>
2024-01-28 00:11:59 +0300

    [feature-8693n7v5p]: fix registration, fix tests

--- internal/tests/service.go
+++ internal/tests/service.go
@@ -3,27 +3,24 @@ package tests
 import (
 	"context"
 
-	tokenService "github.com/OddEer0/task-manager-server/internal/app/service/token_service"
 	userService "github.com/OddEer0/task-manager-server/internal/app/service/user_service"
-	authUsecase "github.com/OddEer0/task-manager-server/internal/app/usecase/auth_usecase"
 	"github.com/OddEer0/task-manager-server/internal/domain/aggregate"
 	"github.com/OddEer0/task-manager-server/internal/infrastructure/storage/mock_repository"
 	"github.com/OddEer0/task-manager-server/internal/presentation/mock"
 )
 
-type MockUseCases struct {
-	AuthUseCase authUsecase.AuthUseCase
+type MockServices struct {
+	UserService userService.Service
 }
 
-func NewUseCases() *MockUseCases {
+func NewServices() *MockServices {
 	userRepo := mock_repository.NewUserRepository()
-	tokenRepo := mock_repository.NewTokenRepository()
 	memUser := mock.NewMockUser()
 	for _, user := range memUser.Users {
 		userRepo.Create(context.Background(), &aggregate.UserAggregate{User: *user})
 	}
 
-	return &MockUseCases{
-		AuthUseCase: authUsecase.New(userService.New(userRepo), tokenService.New(tokenRepo), userRepo),
+	return &MockServices{
+		UserService: userService.New(userRepo),
 	}
 }
